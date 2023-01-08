1ff842b38daa547765c7de2360188be85e88c361
VINCE <117690120+Vincenzoods@users.noreply.github.com>
2023-01-08 18:36:29 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -31,38 +31,57 @@
     $compteBancaire = new CompteBancaire($_POST['solde']);
     // Crée une nouvelle personne en utilisant les valeurs de $_POST['nom'] et $_POST['age']
     $personne = new Personne($_POST['nom'], $_POST['age'], $compteBancaire);
+
+    // $_SESSION['personnes'][] = $personne, cela signifie que $personne sera ajoutée à la fin du tableau $_SESSION['personnes']
+    // ajoute l'objet $personne créé précédemment à la fin du tableau $_SESSION['personnes']. Cela signifie que l'objet sera ajouté à la liste de personnes stockées dans la variable de session
+    // permet de stocker toutes les personnes dans l'ordre où elles ont été créées
     // Ajoute la nouvelle personne à la liste de personnes dans la variable de session
     $_SESSION['personnes'][] = $personne;
   }
 
-  // Initialise la variable $personne_select à null
+
+
   $personne_select = null;
-  // Vérifie si la variable 'personne_select' a été envoyée dans le formulaire
+  // déclare une variable $personne_select et lui affecte la valeur null. Cela signifie que la variable est initialisée avec aucune valeur.
+
+
   if (isset($_POST['personne_select'])) {
-    // Récupère le nom de la personne sélectionnée
+    // vérifie si la variable $_POST['personne_select'] a été définie (si elle a été envoyée dans le formulaire). 
+
+
     $nom_personne_select = $_POST['personne_select'];
-    // Parcours la liste de personnes dans la variable de session
+    // Cette ligne récupère la valeur de $_POST['personne_select'] et la stocke dans la variable $nom_personne_select.
+
+
     foreach ($_SESSION['personnes'] as $personne) {
-      // Si le nom de la personne actuelle correspond au nom de la personne sélectionnée
+      // Cette ligne démarre une boucle foreach, qui parcourera chaque élément du tableau $_SESSION['personnes']. Pour chaque élément, la variable $personne sera définie avec la valeur de l'élément en cours de parcours.
+
+
       if ($personne->nom === $nom_personne_select) {
-        // Définit la personne sélectionnée comme la personne actuelle
+        // Cette ligne vérifie si le nom de la personne actuelle (stockée dans la propriété nom de l'objet $personne) est égal à la valeur de $nom_personne_select. Si c'est le cas, alors le code à l'intérieur des accolades sera exécuté.
+
+
         $personne_select = $personne;
-       
+        // Cette ligne définit la variable $personne_select avec la valeur de $personne. Cela signifie que la personne sélectionnée sera définie comme étant la personne actuelle de la boucle foreach.
+
+
         break;
+        // Cette ligne termine la boucle foreach. Ainsi, une fois que la personne sélectionnée a été trouvée, la boucle s'arrêtera et le reste du code ne sera pas exécuté.
       }
     }
   }
 
 
-  // Si une personne a été sélectionnée
+  // Si une Personne a été sélectionnée ($personne_select est différente de null),
+  // récupère l'objet CompteBancaire associé à la Personne sélectionnée en accédant à la propriété compteBancaire de l'objet $personne_select
   // Vérifie si la variable $personne_select est différente de null
   if ($personne_select !== null) {
     // Récupère l'objet CompteBancaire de la personne sélectionnée
     $compteBancaire = $personne_select->compteBancaire;
 
-   
+
     if (isset($_POST['deposer']) && is_numeric($_POST['deposer'])) {
-      
+
       $deposer = $_POST['deposer'];
       // Si le montant est positif 
       if ($deposer > 0) {
@@ -73,11 +92,11 @@
         echo "Vous ne pouvez pas déposer un montant négatif.";
       }
     }
-    
+
     if (isset($_POST['retirer']) && is_numeric($_POST['retirer'])) {
-      
+
       $retirer = $_POST['retirer'];
-      
+
       if ($retirer > 0) {
         // Si le solde du compte est suffisant 
         if ($compteBancaire->solde >= $retirer) {
@@ -125,10 +144,10 @@
             <input type='submit' value='Valider'>
           </form>
         </td>
-                </tr>";
+        </tr>";
   }
   echo '</table>';
- 
+
   ?>
 </body>
 
